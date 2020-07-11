42b4254ff030f5040ec568424b86e4cdeb35e3d7
CesarFlick <fable2548@gmail.com>
2020-07-11 00:28:33 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,6 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-messageOne.textContent = 'From César'
 
 
 weatherForm.addEventListener('submit', (e) => {
@@ -13,14 +12,14 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    
+
     fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error){
                 messageOne.textContent = data.error
             } else {
                 messageOne.textContent = data.latitude
-                messageTwo.textContent = data.Temperature + "°C"
+                messageTwo.textContent = data.Temperature
             }
         })
     })
